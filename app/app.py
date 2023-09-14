44cc3de5f0ebc090c4c4a93984220fd29bb6e3c7
Pedro Alfonso Martínez Barrón <pedromartinez.dg2@gmail.com>
2023-09-13 18:02:02 -0600

    Trying to add custom background to app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,29 @@
 import os
 import openai
 import streamlit as st
+import base64
 from langchain_reader import langchain_pdf_reader
 from dotenv import load_dotenv, find_dotenv
 from openai_response import get_completion_from_messages
 
 st.title("ASISTENTE MÉDICO :robot_face::medical_symbol:")
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
 
 
 _ = load_dotenv(find_dotenv())
